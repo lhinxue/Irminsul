b1a1762dffa15d2d1a9ba38b1de7f1aa9bf767d1
lhinxue <lhinxue@gmail.com>
2022-12-11 20:12:44 +1300

    wip

--- src/ui/components/iHeader.js
+++ src/ui/components/iHeader.js
@@ -1,11 +1,22 @@
 import { Box } from "@mui/material"
 
 
-export default function iHeader() {
+export default function Header(props) {
+
+    const strTitle = props.title
+    const arrIcons = props.icons ?? []
+    const intZ = props.z ?? 10
+
     return (
-        <Box className="ImsHeader">
-
-
+        <Box className="Header">
+            <Typography className="Title" color={'primary'}>
+                {strTitle}
+            </Typography>
+            {arrIcons.map(btn =>
+                <IconButton color='inherit' onClick={btn.onClick}>
+                    {btn.icon}
+                </IconButton>
+            )}
         </Box>
     )
 }
