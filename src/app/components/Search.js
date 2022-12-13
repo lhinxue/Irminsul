ef2b39741c79691d92b4425717d12405653c2ffb
lhinxue <lhinxue@gmail.com>
2022-12-13 15:48:27 +1300

    wip

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -1,4 +1,5 @@
-import { CloseRounded } from "@mui/icons-material";
+import { ClearOutlined, CloseRounded, SearchOutlined, SwapHorizRounded } from "@mui/icons-material";
+import { Button, IconButton, Input, InputAdornment, MenuItem, Select, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import Collapse from "./Collapse";
 import List from "./List";
@@ -24,8 +25,54 @@ export default function Search(props) {
                     title={'Search'}
                     icons={[{ icon: <CloseRounded fontSize="small" color={'primary'} />, onClick: onClose }]}
                 />
+                <Box sx={{ height: 40, borderBottom: '1px solid silver', backgroundColor: 'white', width: '100%', display: 'flex', flexDirection: 'row' }}>
+                    <Input sx={{ flexGrow: 1, paddingLeft: '20px' }} className="Input" disableUnderline endAdornment={
+                        <InputAdornment>
+                            <IconButton size="small">
+                                <ClearOutlined fontSize="small" />
+                            </IconButton>
+                        </InputAdornment>
+                    } />
+                    <Button>
+                        <SearchOutlined fontSize="small" />
+                    </Button>
+                </Box>
+                <Box sx={{
+                    backgroundColor: 'white',
+                    
+                    width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'flex-end',
+                    '&>div:first-child': {
+                        fontSize: '12px',
+                        marginRight: '5px'
+                    },
+                    '&>div:nth-child(2)': {
+                        fontSize: '13px',
+                        fontVariant: 'small-caps',
+                    },
+                    '&>button': {
+                        width: '13px',
+                        height: '13px',
+                        marginLeft: '3px',
+                        marginRight: '10px',
+                        alignSelf:'center'
+                    },
+                    '& svg': {
+                        fontSize: '12px'
+                    },
+                }}>
+                    <Typography component={'div'} color='secondary'>
+                        Search in
+                    </Typography>
+                    <Typography component={'div'} color='primary'>
+                        Entire Irminsul
+                    </Typography>
+                    <IconButton color="primary">
+                        <SwapHorizRounded fontSize="small" />
+                    </IconButton>
 
-                <List />
+                </Box>
+
+                <List width={'100%'} />
 
             </Box>
         </Collapse>
